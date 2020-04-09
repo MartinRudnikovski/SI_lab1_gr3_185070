ebc044b6ee14e539ad3a06079aaa45396190a643
Martin Rudnkikovski <martinrudnikovski.com@hotmail.com>
2020-04-09 21:21:39 +0200

    Item.java correction

--- Item.java
+++ Item.java
@@ -6,8 +6,46 @@ class Item {
 	//TODO add variable.
 	char tax;
 	//TODO constructor
+	Item(char tax, int id, String name, double price){
+		this.tax = tax;
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
 
 	//TODO setters and getters
+	
+	public voitd setTax(char tax){
+		this.tax = tax;
+	}
+
+	public voitd setId(int id){
+		this.id = id;
+	}
+
+	public voitd setName(String name){
+		this.name = name;
+	}
+	
+	public voitd setPrice(){
+		this.price = price;
+	}	
+
+	public char getTax(){
+		return tax;
+	}
+
+	public int getId(){
+		return id;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public double getPrice(){
+		return price;
+	}
 
 	double taxReturn () {
 		//TODO
